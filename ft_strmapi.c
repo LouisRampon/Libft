8023241d49df60cf4609b372c8b1c663c00b8d1d
madlefou <madlefou97@gmail.com>
2021-11-12 15:16:36 +0100

    euh2

---
+++ ft_strmapi.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc(sizeof(char *) * (ft_strlen(s) + 1));
+	if (!dest || !s || !f)
+		return ('\0');
+	while (s[i])
+	{
+		dest[i] = (f)(i, s[i]);
+		if (!s[i])
+			return ('\0');
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
